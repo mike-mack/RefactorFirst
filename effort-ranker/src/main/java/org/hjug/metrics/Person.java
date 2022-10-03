afd091199f2c1e126ac166ee49b7b3ea24711e9e
Michael MacKinnon <michaelmackinnon@Michaels-MacBook-Pro.local>
2022-10-03 14:45:43 -0400

    test

--- effort-ranker/src/main/java/org/hjug/metrics/Person.java
+++ effort-ranker/src/main/java/org/hjug/metrics/Person.java
@@ -3,6 +3,7 @@ package com.mycompany.app;
 class Person {
     private String firstName;
     private String lastName;
+    private String addr;
 
     public Person(final String firstName, final String lastName) {
         this.firstName = firstName;
